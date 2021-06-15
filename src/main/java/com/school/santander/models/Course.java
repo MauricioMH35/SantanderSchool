c1109b5051cbca1c30e27577b317d841eb50166a
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 14:22:26 -0300

    feat: Start inital implementation Course model

--- src/main/java/com/school/santander/models/Course.java
+++ src/main/java/com/school/santander/models/Course.java
@@ -1,4 +1,24 @@
 package com.school.santander.models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
 public class Course {
+    @Id private String id; // (find - sim)
+    private String name; // (find - sim)
+    private String description; // (find - não)
+    private String tag; // (find - sim)
+    private double workload; // (find - não)
+    @DBRef private Customer[] teachers; // (find - sim)(registry - sim)
+    @DBRef private Customer[] students; // (find - sim)(registry - sim)
 }
