5ce8687cd797a177d148fb9ccd9a976552806065
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 14:18:24 -0300

    feat: Start initial implementations Customer model

--- src/main/java/com/school/santander/models/Customer.java
+++ src/main/java/com/school/santander/models/Customer.java
@@ -1,4 +1,28 @@
 package com.school.santander.models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Document
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
 public class Customer {
+    @Id private String id; // (find - sim)
+    @NotEmpty private String name; // (find - sim)
+    @NotEmpty private String cpf; // usar validation pattern (find - não)
+    @NotEmpty private String email; // usar validation pattern  (find-não)
+    @NotEmpty private String username; // (find - sim)
+    @NotEmpty private String password; // (find - não)
+    private String roles; // user, admin (find -não)
+    private String tag; // student, teacher (find -sim)
+    private LocalDate assign; // data de registro no sistema (find - não)
 }
