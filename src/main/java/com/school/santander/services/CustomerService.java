7c0a1d2dfd54b4610d2a80ec06b7ca9409586696
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 15:04:54 -0300

    feat: Start inital implements Customer

--- src/main/java/com/school/santander/services/CustomerService.java
+++ src/main/java/com/school/santander/services/CustomerService.java
@@ -1,4 +1,15 @@
 package com.school.santander.services;
 
+import com.school.santander.models.Customer;
+import org.springframework.data.domain.Page;
+
 public interface CustomerService {
+    Customer save(Customer customer);
+    Customer findById(String id);
+    Page<Customer> findByNameContains(String name);
+    Customer findByUsername(String username);
+    Page<Customer> findByTag(String tag);
+    Page<Customer> findAll();
+    void updateById(String customerId, Customer customerReplace);
+    void deleteById(String customerId);
 }
--- src/main/java/com/school/santander/services/impls/CustomerServiceImpl.java
+++ src/main/java/com/school/santander/services/impls/CustomerServiceImpl.java
@@ -1,4 +1,55 @@
 package com.school.santander.services.impls;
 
-public class CustomerServiceImpl {
+import com.school.santander.models.Customer;
+import com.school.santander.repositories.CustomerRepository;
+import com.school.santander.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository repository;
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer findById(String id) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> findByNameContains(String name) {
+        return null;
+    }
+
+    @Override
+    public Customer findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> findByTag(String tag) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public void updateById(String customerId, Customer customerReplace) {
+
+    }
+
+    @Override
+    public void deleteById(String customerId) {
+
+    }
 }
