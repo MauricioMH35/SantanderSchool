e59d25b2babc7d770fe55f594e59e7a87a930f12
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 15:07:56 -0300

    feat: Initial implementation Security Config

--- src/main/java/com/school/santander/configs/SecurityConfig.java
+++ src/main/java/com/school/santander/configs/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.school.santander.configs;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                // Habilitar CSRF para a que o token seja requisitado
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String passwordTest = passwordEncoder.encode("dev");
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordTest)
+                .roles("user", "admin")
+                .and()
+
+                .withUser("user")
+                .password(passwordTest)
+                .roles("user");
+    }
 }
