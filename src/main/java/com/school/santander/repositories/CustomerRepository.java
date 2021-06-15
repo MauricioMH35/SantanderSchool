d7ba07f86123345b293ff8d2e9ed53d9e03e5d0f
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 14:27:10 -0300

    feat: Start inital repository Customer repository

--- src/main/java/com/school/santander/repositories/CustomerRepository.java
+++ src/main/java/com/school/santander/repositories/CustomerRepository.java
@@ -1,4 +1,12 @@
 package com.school.santander.repositories;
 
-public class CustomerRepository {
+import com.school.santander.models.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+    List<Customer> findByNameContains(String nameContains);
+    Customer findByUsername(String username);
+    List<Customer> findByTag(String tag);
 }
