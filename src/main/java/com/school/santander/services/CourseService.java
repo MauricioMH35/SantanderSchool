0b7e5bcbb89941193370b5f6d626f6f357ba53f5
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-15 14:33:02 -0300

    feat Start initial document service Course

--- src/main/java/com/school/santander/services/CourseService.java
+++ src/main/java/com/school/santander/services/CourseService.java
@@ -1,4 +1,18 @@
 package com.school.santander.services;
 
-public class CourseService {
+import com.school.santander.models.Course;
+import com.school.santander.models.Customer;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+    Course save(Course course);
+    Course findById(String id);
+    Page<Course> findByNameContains(String name);
+    Page<Course> findByTag(String tag);
+    Page<Customer> listRegistryTeachersCourse(String courseId);
+    Page<Customer> listRegistryStudentsCourse(String courseId);
+    Customer registryTeacherInCourse(String courseId, Customer teacher);
+    Customer registryStudentInCourse(String courseId, Customer student);
+    void updateById(String courseId, Course courseReplace);
+    void deleteById(String courseId);
 }
--- src/main/java/com/school/santander/services/impls/CourseServiceImpl.java
+++ src/main/java/com/school/santander/services/impls/CourseServiceImpl.java
@@ -1,4 +1,66 @@
 package com.school.santander.services.impls;
 
-public class CourseServiceImpl {
+import com.school.santander.models.Course;
+import com.school.santander.models.Customer;
+import com.school.santander.repositories.CourseReposirory;
+import com.school.santander.services.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseReposirory reposirory;
+    
+    @Override
+    public Course save(Course course) {
+        return null;
+    }
+
+    @Override
+    public Course findById(String id) {
+        return null;
+    }
+
+    @Override
+    public Page<Course> findByNameContains(String name) {
+        return null;
+    }
+
+    @Override
+    public Page<Course> findByTag(String tag) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> listRegistryTeachersCourse(String courseId) {
+        return null;
+    }
+
+    @Override
+    public Page<Customer> listRegistryStudentsCourse(String courseId) {
+        return null;
+    }
+
+    @Override
+    public Customer registryTeacherInCourse(String courseId, Customer teacher) {
+        return null;
+    }
+
+    @Override
+    public Customer registryStudentInCourse(String courseId, Customer student) {
+        return null;
+    }
+
+    @Override
+    public void updateById(String courseId, Course courseReplace) {
+
+    }
+
+    @Override
+    public void deleteById(String courseId) {
+
+    }
 }
