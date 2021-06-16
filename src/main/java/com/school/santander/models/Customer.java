6b1d513004efbb3ff4e52017ac49225ce7dc355a
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2021-06-16 00:20:16 -0300

    feat: Attribute modifications to meet the purpose of the application, such as name and attribute removal

--- src/main/java/com/school/santander/models/Customer.java
+++ src/main/java/com/school/santander/models/Customer.java
@@ -1,28 +1,37 @@
 package com.school.santander.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Document
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Customer {
-    @Id private String id; // (find - sim)
-    @NotEmpty private String name; // (find - sim)
-    @NotEmpty private String cpf; // usar validation pattern (find - não)
-    @NotEmpty private String email; // usar validation pattern  (find-não)
-    @NotEmpty private String username; // (find - sim)
-    @NotEmpty private String password; // (find - não)
-    private String roles; // user, admin (find -não)
-    private String tag; // student, teacher (find -sim)
-    private LocalDate assign; // data de registro no sistema (find - não)
+    @Id
+    private String id;
+    @NotEmpty(message = "The name cannot be an empty field.")
+    private String name;
+    @NotEmpty(message = "The CPF cannot be an empty field.")
+    private String cpf;
+    @NotEmpty(message = "The email cannot be an empty field.")
+    private String email;
+    @NotEmpty(message = "The username cannot be an empty field.")
+    private String username;
+    @NotEmpty(message = "The password cannot be an empty field.")
+    private String password;
+    private String roles;
+    private String tag;
+    private LocalDate assign;
+    @DBRef private Course registration;
 }
